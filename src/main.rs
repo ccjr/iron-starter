7e583b16356b6c2001122d3a7e7f36cae41162ad
Cloves Carneiro Jr <cloves.carneiro@livingsocial.com>
2016-02-09 10:19:29 -0500

    Using daemonize

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+extern crate daemonize;
 extern crate iron;
 
+use daemonize::{Daemonize};
 use iron::prelude::*;
 use iron::status;
 
@@ -8,6 +10,15 @@ fn main() {
         Ok(Response::with((status::Ok, "Hello World!")))
     }
 
-    Iron::new(hello_world).http("localhost:3000").unwrap();
-    println!("On 3000");
+    let daemonize = Daemonize::new()
+        .pid_file("tmp/iron-starter.pid");
+
+    match daemonize.start() {
+         Ok(_) => {
+             println!("On 3000");
+             Iron::new(hello_world).http("localhost:3000").unwrap();
+         },
+         Err(e) => println!("{}", e),
+     }
+
 }
